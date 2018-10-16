94e9c84515a2c339e035ad2da7ddbc36c4508151
Pascal Maissen <pascal.maissen@reconline.com>
2018-10-16 23:24:07 +0200

    initialization

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # thingy-client-red
 Client using the thingy API
+
+## Getting started
+1. Install Angular CLI `npm install -g @angular/cli`
+2. Run `npm install`
+
+## Run
+`ng serve`
+
+Navigate to `localhost:4200`
---
+++ src/app/data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(public http:HttpClient) { }
+
+  getLight() {
+    return this.http.get('http://localhost:8000/api/v1/light/latest')
+  }
+
+  getHumidity() {
+    return this.http.get('http://localhost:8000/api/v1/humidity/latest')
+  }
+
+  getTemperature() {
+    return this.http.get('http://localhost:8000/api/v1/temperature/latest')
+  }
+
+  getAirQuality() {
+    return this.http.get('http://localhost:8000/api/v1/air_quality/latest')
+  }
+
+}
