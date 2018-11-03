e2c91b01b68a6aec3ebbc42df96907a77236c264
Timo Spring <timo.spring@students.unibe.ch>
2018-11-03 09:41:04 +0100

    Design update for air quality and added values for thingy2 and history data

--- src/app/airquality/air.component.ts
+++ src/app/airquality/air.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Air } from './air.model';
 import { AirService } from './air.service';
 import { Subscription, interval } from 'rxjs';
@@ -10,17 +10,41 @@ import { Subscription, interval } from 'rxjs';
     styleUrls: ['./air.component.css']
 })
 
-export class AirComponent implements OnInit {
-    airs: any;
-    defaultInterval = 1;
+export class AirComponent implements OnInit, OnDestroy {
+    airs1: any;
+    airs2: any; 
+    latestAir1: any; 
+    latestAir2: any;
+    lookUpRange = '10';
+
+
+    private airSub1: Subscription;
+    private airSub2: Subscription;
 
     constructor(public airService: AirService) {}
 
     ngOnInit() {
-        interval(this.defaultInterval * 1000).subscribe(x => {
-            this.airService.getAirQuality().subscribe(response => {
-                this.airs = response;
+        interval(1000).subscribe(x => {
+            this.airService.getAirQuality1(this.lookUpRange); 
+            this.airSub1 = this.airService.getAirUpdateListener1()
+            .subscribe((temperatures: Air[]) => {
+                this.latestAir1 = temperatures[0];
+                this.airs1 = temperatures; 
             });
-        });
+        })
+        interval(1000).subscribe(x => {
+            this.airService.getAirQuality2(this.lookUpRange); 
+            this.airSub2 = this.airService.getAirUpdateListener2()
+            .subscribe((temperatures: Air[]) => {
+                this.latestAir2 = temperatures[0];
+                this.airs2 = temperatures; 
+            });
+        })
+    }
+
+    ngOnDestroy() {
+        this.airSub1.unsubscribe();
+        this.airSub2.unsubscribe(); 
     }
 }
+
--- src/app/airquality/air.model.ts
+++ src/app/airquality/air.model.ts
@@ -1,5 +1,6 @@
 export interface Air {
     time: string;
+    device: string;
     eco2: number;
     tvoc: number;
 }
