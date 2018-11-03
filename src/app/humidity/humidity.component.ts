c055b40a5dccce4194891e156b979e35f739c608
Timo Spring <timo.spring@students.unibe.ch>
2018-11-03 10:03:58 +0100

    Design update for humidity with thingy 2 values and humidity history

--- src/app/humidity/humidity.component.ts
+++ src/app/humidity/humidity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Humidity } from './humidity.model';
 import { HumidityService } from './humidity.service';
 import { Subscription, interval } from 'rxjs';
@@ -10,17 +10,40 @@ import { Subscription, interval } from 'rxjs';
     styleUrls: ['./humidity.component.css']
 })
 
-export class HumidityComponent implements OnInit {
-    humidities: any;
-    defaultInterval = 1;
+export class HumidityComponent implements OnInit, OnDestroy {
+    humidities1: any;
+    humidities2: any;
+    latest1: any; 
+    latest2: any;
+    lookUpRange = '10'; // in seconds
+
+    private humidSub1: Subscription;
+    private humidSub2: Subscription;
+
 
     constructor(public humidityService: HumidityService) {}
 
     ngOnInit() {
-        interval(this.defaultInterval * 1000).subscribe(x => {
-            this.humidityService.getHumidity().subscribe(response => {
-                this.humidities = response;
+        interval(1000).subscribe(x => {
+            this.humidityService.getHumidities1(this.lookUpRange); 
+            this.humidSub1 = this.humidityService.getHumidityUpdateListener1()
+            .subscribe((humidities1: Humidity[]) => {
+                this.latest1 = humidities1[0];
+                this.humidities1 = humidities1; 
             });
-        });
+        })
+        interval(1000).subscribe(x => {
+            this.humidityService.getHumidities2(this.lookUpRange); 
+            this.humidSub2 = this.humidityService.getHumidityUpdateListener2()
+            .subscribe((humidities2: Humidity[]) => {
+                this.latest2 = humidities2[0];
+                this.humidities2 = humidities2; 
+            });
+        })
+    }
+
+    ngOnDestroy() {
+        this.humidSub1.unsubscribe();
+        this.humidSub2.unsubscribe(); 
     }
 }
--- src/app/humidity/humidity.service.ts
+++ src/app/humidity/humidity.service.ts
@@ -1,14 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Humidity } from './humidity.model';
+import { Subject } from "rxjs"; 
 
 @Injectable({providedIn: 'root'})
 export class HumidityService {
-    defaultInterval = 1;
+    private humidities1: Humidity[] = []; 
+    private humidities2: Humidity[] = []; 
+
+    private humiditiesUpdated1 = new Subject<Humidity[]>(); 
+    private humiditiesUpdated2 = new Subject<Humidity[]>(); 
+
 
     constructor(private http: HttpClient) { }
 
-    getHumidity() {
-        return this.http.get(`${environment.api}/api/v1/Thingy1/humidity`);
+    getHumidities1(rangeInSeconds) {
+        this.http
+            .get<Humidity[]>(
+                `${environment.api}/api/v1/Thingy1/humidity/${rangeInSeconds}`
+        ).subscribe((response) => {
+            this.humidities1 = response;
+            this.humiditiesUpdated1.next([...this.humidities1]);
+        })
+    }
+
+    getHumidities2(rangeInSeconds) {
+        this.http
+            .get<Humidity[]>(
+                `${environment.api}/api/v1/Thingy2/humidity/${rangeInSeconds}`
+        ).subscribe((response) => {
+            this.humidities2 = response;
+            this.humiditiesUpdated2.next([...this.humidities2]);
+        })
+    }
+
+    getHumidityUpdateListener1() {
+        return this.humiditiesUpdated1.asObservable(); 
+    }
+
+    getHumidityUpdateListener2() {
+        return this.humiditiesUpdated2.asObservable(); 
     }
 }
