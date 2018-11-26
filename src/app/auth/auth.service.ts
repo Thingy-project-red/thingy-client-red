872adaf348def4748a3e01c63c11da76201acfdd
Timo Spring <timo.spring@students.unibe.ch>
2018-11-26 10:46:14 +0100

    Caching the token and auto-login if token is not yet expired

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
     private isAuthenticated = false;
     private token: string;
     private authStatusListener = new Subject<boolean>();
-    private tokenTimer: any; 
+    private tokenTimer: any;
 
     constructor(private http: HttpClient, private router: Router) { }
 
@@ -22,39 +22,52 @@ export class AuthService {
         this.http.post(
             `${environment.api}/api/v1/users`, authData)
             .subscribe(response => {
-                console.log("User created successfully"); 
+                console.log("User created successfully");
                 this.router.navigate(['/dashboard']);
-                /*
-                if (response) {
-                    this.login(user, password); 
-                }*/    
-        })
+            })
     }
 
     login(username: string, password: string) {
         const authData: AuthData = { name: username, password: password };
-        this.http.post<{token: string, expiresIn: number}>(
+        this.http.post<{ token: string, expiresIn: number }>(
             `${environment.api}/api/v1/auth`, authData)
             .subscribe(response => {
-                const token = response.token; 
+                const token = response.token;
                 this.token = token;
                 if (token) {
-                    let expiresInDuration = response.expiresIn; 
-                    this.tokenTimer = setTimeout(() => {
-                        this.logout(); 
-                    }, expiresInDuration * 1000); // timeout in ms  
+                    const expiresInDuration = response.expiresIn;
+                    this.setAuthenticationTimer(expiresInDuration);
                     this.isAuthenticated = true;
                     this.authStatusListener.next(true);
+                    const now = new Date();
+                    const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+                    this.cacheAuthToken(token, expirationDate);
                     this.router.navigate(['/dashboard']);
                 }
             })
+    }
+
+    autoAuth() {
+        const authData = this.getCachedAuthData();
+        if (!authData) {
+            return;
+        }
+        const now = new Date();
+        const expiresIn = authData.expirationDate.getTime() - now.getTime();
+        if (expiresIn > 0) {
+            this.token = authData.token;
+            this.isAuthenticated = true;
+            this.setAuthenticationTimer(expiresIn / 1000);
+            this.authStatusListener.next(true);
+        }
     }
 
     logout() {
         this.token = null;
         this.isAuthenticated = false;
         this.authStatusListener.next(false);
-        clearTimeout(this.tokenTimer); 
+        clearTimeout(this.tokenTimer);
+        this.clearAuthCache();
         this.router.navigate(['/login']);
 
     }
@@ -69,6 +82,37 @@ export class AuthService {
 
     getAuthStatusListener() {
         return this.authStatusListener.asObservable();
+    }
+
+
+    private cacheAuthToken(token: string, expirationDate: Date) {
+        localStorage.setItem("token", token);
+        localStorage.setItem("expiration", expirationDate.toISOString());
+    }
+
+    private clearAuthCache() {
+        localStorage.removeItem("token");
+        localStorage.removeItem("expiration");
+    }
+
+    private getCachedAuthData() {
+        const token = localStorage.getItem("token");
+        const expirationDate = localStorage.getItem("expiration");
+
+        if (!token || !expirationDate) {
+            return;
+        }
+
+        return {
+            token: token,
+            expirationDate: new Date(expirationDate)
+        };
+    }
+
+    private setAuthenticationTimer(expiresInDuration: number) {
+        this.tokenTimer = setTimeout(() => {
+            this.logout();
+        }, expiresInDuration * 1000); // timeout in ms  
     }
 
 
