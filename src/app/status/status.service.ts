46d908fcc00880e27de179f97d20bb3a5b0ef510
Timo Spring <timo.spring@students.unibe.ch>
2018-11-26 14:24:45 +0100

    Refactored status component

--- src/app/status/status.service.ts
+++ src/app/status/status.service.ts
@@ -7,41 +7,32 @@ import { Status } from "./status.model";
 @Injectable({ providedIn: 'root' })
 export class StatusService {
 
-    private status1: Status;
-    private status2: Status;
-
+    private status: Status;
     private statusUpdated1 = new Subject<Status>(); 
     private statusUpdated2 = new Subject<Status>(); 
 
     constructor(private http: HttpClient){}
 
-    getStatus1() {
+    getStatus(device) {
         this.http
         .get<Status>(
-            `${environment.api}/api/v1/Thingy1/status`,
+            `${environment.api}/api/v1/${device}/status`,
         ).subscribe((response) => {
-            this.status1 = response;
-            this.statusUpdated1.next(this.status1);
+            this.status = response;
+            if(device == "Thingy1"){
+                this.statusUpdated1.next(this.status);
+            }else{
+                this.statusUpdated2.next(this.status);
+            }
         })
 
     }
 
-    getStatus2() {
-        this.http
-        .get<Status>(
-            `${environment.api}/api/v1/Thingy2/status`,
-        ).subscribe((response) => {
-            this.status2 = response;
-            this.statusUpdated2.next(this.status2);
-        })
-
-    }
-
-    getStatusUpdateListener1() {
-        return this.statusUpdated1.asObservable();
-    }
-
-    getStatusUpdateListener2() {
-        return this.statusUpdated2.asObservable();
+    getStatusUpdateListener(device) {
+        if(device == "Thingy1"){
+            return this.statusUpdated1.asObservable();
+        }else{
+            return this.statusUpdated2.asObservable();
+        }
     }
 }
