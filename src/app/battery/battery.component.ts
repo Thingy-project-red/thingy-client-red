1ac1a6697ba7ecb5f14c21517ddb316cbed4bd1b
Pascal Maissen <pascal.maissen@reconline.com>
2018-12-16 11:03:59 +0100

    battery dashboard error handling

--- src/app/battery/battery.component.ts
+++ src/app/battery/battery.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { WebsocketService } from '../websocket/websocket.service';
 import { Battery } from './battery.model';
+import { ErrorService } from '../errors/error.service';
 
 @Component({
     selector: 'app-battery-latest',
@@ -14,7 +15,7 @@ export class BatteryComponent implements OnInit, OnDestroy {
 
     private subscription: Subscription;
 
-    constructor(public websocketService: WebsocketService) { }
+    constructor(public websocketService: WebsocketService, private errorService: ErrorService) { }
 
     ngOnInit() {
         this.subscription = this.websocketService.batteries
@@ -22,6 +23,9 @@ export class BatteryComponent implements OnInit, OnDestroy {
             if (battery.device === this.device) {
                 this.batteryLevel = battery.battery_level;
             }
+        },
+        (error) => {
+            this.errorService.addError('Battery: could not load status of battery', new Date());
         });
     }
 
