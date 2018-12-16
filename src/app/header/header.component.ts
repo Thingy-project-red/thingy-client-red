581c313fb10ad415ac7b5df745cb876c000de90a
Pascal Maissen <pascal.maissen@reconline.com>
2018-12-16 10:36:28 +0100

    icon button for handling erros

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit, OnDestroy, Inject } from "@angular/core";
 import { AuthService } from "../auth/auth.service";
 import { Subscription } from "rxjs";
 import { UserService } from "../users/user.service";
+import { ErrorService } from "../errors/error.service";
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: "app-header",
@@ -16,7 +18,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   username: string;
   isAdmin: boolean;
 
-  constructor(private authService: AuthService, private userService: UserService) {}
+  constructor(private authService: AuthService, private userService: UserService, private errorService: ErrorService, public dialog: MatDialog) {}
 
   ngOnInit() {
     this.userIsAuthenticated = this.authService.getIsAuth();
@@ -43,6 +45,16 @@ export class HeaderComponent implements OnInit, OnDestroy {
       }); 
   }
 
+  openDialog(error): void {
+    const dialogRef = this.dialog.open(ErrorDialog, {
+      width: '400px',
+      data: {error: error}
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.errorService.removeError(result);
+    });
+  }
+
   onLogout() {
     this.authService.logout();
     this.username = null;
@@ -53,4 +65,27 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.adminListenerSubs.unsubscribe(); 
     this.usernameSubs.unsubscribe(); 
   }
+}
+
+@Component({
+  selector: 'error-dialog',
+  template: `<h1 mat-dialog-title>Error</h1>
+  <div mat-dialog-content>
+    <p>{{data.error}}</p>
+  </div>
+  <div mat-dialog-actions>
+    <button mat-raised-button color="primary" (click)="onNoClick()">Cancel</button>
+    <button mat-raised-button color="primary" [mat-dialog-close]="data.error" cdkFocusInitial>Dismiss Error</button>
+  </div>`,
+})
+export class ErrorDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<ErrorDialog>,
+    @Inject(MAT_DIALOG_DATA) public data) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
